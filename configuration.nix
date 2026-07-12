b4b2431a5c560c3f75f12bb472b539e6395609e6
Martin Paul Eve <martin@eve.gd>
2026-07-12 09:36:23 +0100

    fix(openvpn3): enable service for openvpn3

--- configuration.nix
+++ configuration.nix
@@ -216,6 +216,8 @@ in
     enable = true; 
     polkitPolicyOwners = [ "martin" ];
   };
+
+  programs.openvpn3.enable = true;
   
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
